9acf7e518681574f97cf8bf9a6c7fe454eb1c609
Victoria <victoria.babchenko30@gmail.com>
2023-12-21 11:17:58 +0200

    Added InsertIntoABinarySearchTree another solution option

--- src/InsertIntoABinarySearchTree.java
+++ src/InsertIntoABinarySearchTree.java
@@ -3,31 +3,41 @@ public class InsertIntoABinarySearchTree {
         if(root == null){
             return new TreeNode(val);
         }
-
-        TreeNode temp = root;
-
-        while (root != null){
-            if(root.val > val){
-                if(root.left != null){
-                    root = root.left;
-                }
-                else{
-                    root.left = new TreeNode(val);
-                    break;
-                }
-            }
-            else {
-                if(root.right != null){
-                    root = root.right;
-                }
-                else{
-                    root.right = new TreeNode(val);
-                    break;
-                }
-            }
+        if(root.val > val){
+            root.left = insertIntoBST(root.left, val);
         }
+        if(root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
 
-        return temp;
-
+//        if(root == null){
+//            return new TreeNode(val);
+//        }
+//
+//        TreeNode temp = root;
+//
+//        while (root != null){
+//            if(root.val > val){
+//                if(root.left != null){
+//                    root = root.left;
+//                }
+//                else{
+//                    root.left = new TreeNode(val);
+//                    break;
+//                }
+//            }
+//            else {
+//                if(root.right != null){
+//                    root = root.right;
+//                }
+//                else{
+//                    root.right = new TreeNode(val);
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return temp;
     }
 }
