813305e7f4159f4b2ca575b35b8decbffbee8088
Victoria <victoria.babchenko30@gmail.com>
2023-12-24 00:35:23 +0200

    added MaximumDepthOfBinaryTree

--- src/solutions/BinarySearchTreeToGreaterSumTree.java
+++ src/solutions/BinarySearchTreeToGreaterSumTree.java
@@ -4,9 +4,9 @@ public class BinarySearchTreeToGreaterSumTree {
     int sum = 0;
     public TreeNode bstToGst(TreeNode root) {
         if(root != null){
+            bstToGst(root.right);
             sum += root.val;
             root.val = sum;
-            bstToGst(root.right);
             bstToGst(root.left);
         }
         return root;
