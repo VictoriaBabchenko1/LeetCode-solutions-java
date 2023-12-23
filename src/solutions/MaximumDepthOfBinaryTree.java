62381a228186563220db0a0fc15d278b038f6825
Victoria <victoria.babchenko30@gmail.com>
2023-12-24 00:37:25 +0200

    added MaximumDepthOfBinaryTree

--- src/solutions/MaximumDepthOfBinaryTree.java
+++ src/solutions/MaximumDepthOfBinaryTree.java
@@ -2,13 +2,7 @@ package solutions;
 
 public class MaximumDepthOfBinaryTree {
     public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-
-        return Math.max(left, right) + 1;
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
