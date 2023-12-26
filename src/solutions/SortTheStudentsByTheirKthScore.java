60bc8e71be5467567e180eee133f94444fbb8d12
Victoria <victoria.babchenko30@gmail.com>
2023-12-26 04:44:52 +0200

    added SortTheStudentsByTheirKthScore

--- src/solutions/SortTheStudentsByTheirKthScore.java
+++ src/solutions/SortTheStudentsByTheirKthScore.java
@@ -1,7 +1,6 @@
 package solutions;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class SortTheStudentsByTheirKthScore {
     public int[][] sortTheStudents(int[][] score, int k) {
