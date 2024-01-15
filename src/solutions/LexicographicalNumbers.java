cf221f97be04d4ac616edb8c807aef8051101e76
Victoria <victoria.babchenko30@gmail.com>
2024-01-15 06:24:53 +0200

    added LexicographicalNumbers

--- src/solutions/LexicographicalNumbers.java
+++ src/solutions/LexicographicalNumbers.java
@@ -8,14 +8,10 @@ import java.util.List;
 public class LexicographicalNumbers {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> result = new ArrayList<>();
-        String[] strs = new String[n];
         for(int i = 1; i <= n; i++){
-            strs[i - 1] = Integer.toString(i);
+            result.add(i);
         }
-        Arrays.sort(strs);
-        for(String str : strs){
-            result.add(Integer.parseInt(str));
-        }
+        result.sort((a, b) -> (Integer.toString(a).compareTo(Integer.toString(b))));
         return result;
     }
 }
