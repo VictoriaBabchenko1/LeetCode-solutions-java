61ac82388244917be46bfd17bb52ca2b091c1a9d
Victoria <victoria.babchenko30@gmail.com>
2024-01-17 11:33:03 +0200

    added UniqueNumberOfOccurrences

--- src/solutions/UniqueNumberOfOccurrences.java
+++ src/solutions/UniqueNumberOfOccurrences.java
@@ -16,5 +16,22 @@ public class UniqueNumberOfOccurrences {
         Set<Integer> set = new HashSet<>(map.values());
 
         return set.size() == map.size();
+
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int val : arr){
+//            map.put(val, map.getOrDefault(val, 0) + 1);
+//        }
+//
+//        List<Integer> list = new ArrayList<>();
+//        for (Integer val : map.values()){
+//            if(list.contains(val)){
+//                return false;
+//            }
+//            else {
+//                list.add(val);
+//            }
+//        }
+//
+//        return true;
     }
 }
