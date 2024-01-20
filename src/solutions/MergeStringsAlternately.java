7e46664dc46561998fc7938f49d1205e7c530809
Victoria <victoria.babchenko30@gmail.com>
2024-01-20 05:47:25 +0200

    added MergeStringsAlternately

--- src/solutions/MergeStringsAlternately.java
+++ src/solutions/MergeStringsAlternately.java
@@ -2,17 +2,32 @@ package solutions;
 
 public class MergeStringsAlternately {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder result = new StringBuilder();
+        char[] result = new char[word1.length() + word2.length()];
+        int index = 0;
 
-        for(int i = 0; i < word1.length() || i < word2.length(); i++){
-            if(i < word1.length()){
-                result.append(word1.charAt(i));
+        for(int i = 0; i < word1.length() || i < word2.length(); i++) {
+            if (i < word1.length()) {
+                result[index++] = word1.charAt(i);
             }
-            if(i < word2.length()){
-                result.append(word2.charAt(i));
+            if (i < word2.length()) {
+                result[index++] = word2.charAt(i);
             }
         }
 
-        return result.toString();
+        return new String(result);
+
+
+//        StringBuilder result = new StringBuilder();
+//
+//        for(int i = 0; i < word1.length() || i < word2.length(); i++){
+//            if(i < word1.length()){
+//                result.append(word1.charAt(i));
+//            }
+//            if(i < word2.length()){
+//                result.append(word2.charAt(i));
+//            }
+//        }
+//
+//        return result.toString();
     }
 }
