faa3cb2bb442a797bda67f00d6d26b3c15781dc2
Victoria <victoria.babchenko30@gmail.com>
2024-01-21 15:23:54 +0200

    added Maximum69Number

--- src/solutions/Maximum69Number.java
+++ src/solutions/Maximum69Number.java
@@ -2,15 +2,28 @@ package solutions;
 
 public class Maximum69Number {
     public int maximum69Number (int num) {
-        StringBuilder res = new StringBuilder("" + num);
+        String str = Integer.toString(num);
+        char arr[] = str.toCharArray();
 
-        for(int i = 0; i < res.length(); i++){
-            if(res.charAt(i) == '6'){
-                res.setCharAt(i, '9');
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == '6'){
+                arr[i] = '9';
                 break;
             }
         }
 
-        return Integer.parseInt(res.toString());
+        return Integer.parseInt(new String(arr));
+
+
+//        StringBuilder res = new StringBuilder("" + num);
+//
+//        for(int i = 0; i < res.length(); i++){
+//            if(res.charAt(i) == '6'){
+//                res.setCharAt(i, '9');
+//                break;
+//            }
+//        }
+//
+//        return Integer.parseInt(res.toString());
     }
 }
