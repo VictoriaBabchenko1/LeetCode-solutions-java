aacb9cde95208cbe7e41d6e057f437fafec39cc8
Victoria <victoria.babchenko30@gmail.com>
2024-01-31 14:15:24 +0200

    added DailyTemperatures

--- src/solutions/DailyTemperatures.java
+++ src/solutions/DailyTemperatures.java
@@ -1,23 +1,38 @@
 package solutions;
 
+import java.util.Stack;
+
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>(); // Stack to store indices
 
         for (int i = 0; i < temperatures.length; i++) {
-            int days = 0;
-            for (int j = i; j < temperatures.length; j++) {
-                days++;
-                if(temperatures[j] > temperatures[i]){
-                    result[i] = days;
-                    break;
-                }
-                else if(j == temperatures.length - 1){
-                    result[i] = 0;
-                }
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                result[prevIndex] = i - prevIndex;
             }
+            stack.push(i);
         }
 
         return result;
+
+//        int[] result = new int[temperatures.length];
+//
+//        for (int i = 0; i < temperatures.length; i++) {
+//            int days = 0;
+//            for (int j = i; j < temperatures.length; j++) {
+//                days++;
+//                if(temperatures[j] > temperatures[i]){
+//                    result[i] = days;
+//                    break;
+//                }
+//                else if(j == temperatures.length - 1){
+//                    result[i] = 0;
+//                }
+//            }
+//        }
+//
+//        return result;
     }
 }
