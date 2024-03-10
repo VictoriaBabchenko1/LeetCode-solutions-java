a727d9ec79f0db0e8bef8dafdd448fbb6ecc99b1
Victoria <victoria.babchenko30@gmail.com>
2024-03-10 12:14:25 +0200

    added CheckIfAStringIsAnAcronymOfWords better variant

--- src/solutions/CheckIfAStringIsAnAcronymOfWords.java
+++ src/solutions/CheckIfAStringIsAnAcronymOfWords.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 public class CheckIfAStringIsAnAcronymOfWords {
     public boolean isAcronym(List<String> words, String s) {
-        if(words.size() != s.length()){
-            return false;
+        StringBuilder sb = new StringBuilder();
+        for(String word : words) {
+            sb.append(word.charAt(0));
         }
+        return sb.toString().equals(s);
 
-        for(int i = 0; i < words.size(); i++){
-            if(words.get(i).charAt(0) != s.charAt(i)){
-                return false;
-            }
-        }
-
-        return true;
+//        if(words.size() != s.length()){
+//            return false;
+//        }
+//
+//        for(int i = 0; i < words.size(); i++){
+//            if(words.get(i).charAt(0) != s.charAt(i)){
+//                return false;
+//            }
+//        }
+//
+//        return true;
     }
 }
