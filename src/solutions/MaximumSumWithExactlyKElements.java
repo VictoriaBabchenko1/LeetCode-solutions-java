2391ac4c2318fa886250a170d2295322ec69e76f
Victoria <victoria.babchenko30@gmail.com>
2024-03-17 12:27:06 +0200

    added MaximumSumWithExactlyKElements

--- src/solutions/MaximumSumWithExactlyKElements.java
+++ src/solutions/MaximumSumWithExactlyKElements.java
@@ -4,11 +4,25 @@ import java.util.Arrays;
 
 public class MaximumSumWithExactlyKElements {
     public int maximizeSum(int[] nums, int k) {
-        Arrays.sort(nums);
-        int result = nums[nums.length - 1];
-        for(int i = 1; i < k; i++) {
-            result += nums[nums.length - 1] + i;
+//        Arrays.sort(nums);
+//        int result = nums[nums.length - 1];
+//        for(int i = 1; i < k; i++) {
+//            result += nums[nums.length - 1] + i;
+//        }
+//        return result;
+
+        int max = 0;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
         }
+
+        int result = 0;
+        for(int i = 1; i < k; i++) {
+            result += max + i;
+        }
+
         return result;
     }
 }
